6cacb03910c73b371caaaa9118805621e06c06f5
RiqueISantos <riquesantosoliveira@gmail.com>
2025-06-17 16:25:08 -0300

    Criando mock de endpoints

--- src/main/java/dev/henrique/CadastroDeAlunos/Alunos/AlunosController.java
+++ src/main/java/dev/henrique/CadastroDeAlunos/Alunos/AlunosController.java
@@ -1,8 +1,6 @@
 package dev.henrique.CadastroDeAlunos.Alunos;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,35 @@ public class AlunosController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota de cadastro";
+    }
+
+    //Adicionar Aluno
+    @PostMapping("/criar")
+    public String criarAluno(){
+        return "Aluno criado";
+    }
+
+    //Mostrar todos os alunos
+    @GetMapping("/todos")
+    public String mostrarTodosOsAlunos(){
+        return "Mostrar aluno";
+    }
+
+    //Mostrar aluno por ID
+    @GetMapping("/todosID")
+    public String mostrarTodosOsAlunosPorId(){
+        return "Mostrar aluno por id";
+    }
+
+    //Alterar dados dos alunos por ID
+    @PutMapping("/alterarID")
+    public String alterarAlunoPorId(){
+        return "Alterar Aluno por ID";
+    }
+
+    //Deletar aluno pelo ID
+    @DeleteMapping("/deletarID")
+    public String deletarAlunoPorId(){
+        return "Aluno deletado por ID";
     }
 }
